2a09e1cab63b0503ed623140bd49b34288fc1e9e
murat-polat <muratsdepo@gmail.com>
2022-03-05 23:52:55 +0100

    add ed price to the models

--- cafeteria/menu/admin.py
+++ cafeteria/menu/admin.py
@@ -5,7 +5,7 @@ from .models import Question, Choice
 admin.site.site_header = "Cafeteria Admin"
 admin.site.site_title = "cafeteria Admin Area"
 
-
+## Here we can add and manage new questions/choices in Django Admin site
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
